474b45bb9603622cb251fd73e93eafce89c4d389
oumarknt31 <153241801+oumarknt31@users.noreply.github.com>
2023-12-10 13:07:50 -0500

    Delete questionquiz.cpp

--- questionquiz.cpp
+++
@@ -1,9 +0,0 @@
-#include "questionquiz.h"
-
-QuestionQuiz::QuestionQuiz() {}
-
-QuestionQuiz::QuestionQuiz(const QString& question, const QVector<QString>& options, int correct) {
-    questionText = question;
-    this->options = options;
-    correctOption = correct;
-}
